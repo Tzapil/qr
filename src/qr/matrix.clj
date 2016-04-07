29e6421603a150fd205bd1d96c2eedfbc91ea99a
du.kulaevskiy <thhwitcher@gmail.com>
2016-04-07 15:33:55 +0300

    tests_matrix #2

--- src/qr/matrix.clj
+++ src/qr/matrix.clj
@@ -5,204 +5,208 @@
 (def size (+ (* (- version 1) 4) 21))
 
 (def finding-pattern
-	[[3 3 3 3 3 3 3]
-	 [3 2 2 2 2 2 3]
-	 [3 2 3 3 3 2 3]
-	 [3 2 3 3 3 2 3]
-	 [3 2 3 3 3 2 3]
-	 [3 2 2 2 2 2 3]
-	 [3 3 3 3 3 3 3]])
+    [[3 3 3 3 3 3 3]
+     [3 2 2 2 2 2 3]
+     [3 2 3 3 3 2 3]
+     [3 2 3 3 3 2 3]
+     [3 2 3 3 3 2 3]
+     [3 2 2 2 2 2 3]
+     [3 3 3 3 3 3 3]])
 
  (defn build-template
- 	[size]
- 	(vec (repeat size (vec (repeat size 9)))))
+     [size]
+     (vec (repeat size (vec (repeat size 9)))))
+
+ (defn equal
+ 	[f1 f2]
+	(every? true? (map #(= %1 %2) f1 f2)))
 
 (defn set-pixel
-	[x y value field]
-	(assoc-in field [y x] value)) 
+    [x y value field]
+    (assoc-in field [y x] value)) 
 
 (defn get-pixel
-	[x y field]
-	(get-in field [y x])) 
+    [x y field]
+    (get-in field [y x])) 
 
 (defn draw-pattern-flat
-	[x y line field]
-	(let [size-x (count line)]
-		(loop [iterator-x 0
-			   new-field field]
-			   (if (< iterator-x size-x)
-					(recur (inc iterator-x) (set-pixel (+ x iterator-x) y (get line iterator-x) new-field))
-					new-field))))
+    [x y line field]
+    (let [size-x (count line)]
+        (loop [iterator-x 0
+               new-field field]
+               (if (< iterator-x size-x)
+                    (recur (inc iterator-x) (set-pixel (+ x iterator-x) y (get line iterator-x) new-field))
+                    new-field))))
 
 (defn draw-pattern
-	[x y pattern field]
-	(let [size-y (count pattern)]
-		(loop [iterator-y 0
-			   new-field field]
-			(if (< iterator-y size-y)
-				(recur (inc iterator-y) (draw-pattern-flat x (+ y iterator-y) (get pattern iterator-y) new-field))
-				new-field))))
+    [x y pattern field]
+    (let [size-y (count pattern)]
+        (loop [iterator-y 0
+               new-field field]
+            (if (< iterator-y size-y)
+                (recur (inc iterator-y) (draw-pattern-flat x (+ y iterator-y) (get pattern iterator-y) new-field))
+                new-field))))
 
 (defn draw-line-x
-	[x y length value field]
-	(draw-pattern x y [(vec (repeat length value))] field))
+    [x y length value field]
+    (draw-pattern x y [(vec (repeat length value))] field))
 
 (defn draw-line-y
-	[x y length value field]
-	(draw-pattern x y (vec (repeat length [value])) field))
+    [x y length value field]
+    (draw-pattern x y (vec (repeat length [value])) field))
 
 (defn add-left-top-fp
-	[field]
-	(->> field
-		(draw-pattern 0 0 finding-pattern)
-		(draw-line-x 0 7 8 2)
-		(draw-line-y 7 0 8 2)))
+    [field]
+    (->> field
+        (draw-pattern 0 0 finding-pattern)
+        (draw-line-x 0 7 8 2)
+        (draw-line-y 7 0 8 2)))
 
 (defn add-right-top-fp
-	[field]
-	(let [x-pos (- size 7)]
-		(->> field
-			(draw-pattern x-pos 0 finding-pattern)
-			(draw-line-x (dec x-pos) 7 8 2)
-			(draw-line-y (dec x-pos) 0 8 2))))
+    [field]
+    (let [x-pos (- size 7)]
+        (->> field
+            (draw-pattern x-pos 0 finding-pattern)
+            (draw-line-x (dec x-pos) 7 8 2)
+            (draw-line-y (dec x-pos) 0 8 2))))
 
 (defn add-left-bottom-fp
-	[field]
-	(let [y-pos (- size 7)]
-		(->> field
-			(draw-pattern 0 y-pos finding-pattern)
-			(draw-line-x 0 (dec y-pos) 8 2)
-			(draw-line-y 7 (dec y-pos) 8 2))))
+    [field]
+    (let [y-pos (- size 7)]
+        (->> field
+            (draw-pattern 0 y-pos finding-pattern)
+            (draw-line-x 0 (dec y-pos) 8 2)
+            (draw-line-y 7 (dec y-pos) 8 2))))
 
 (defn- counter []  
   (let [tick (atom 0)]
     #(swap! tick (fn [n] (+ (mod (inc n) 2) 2)))))
 
 (defn add-timing-patterns
-	[field]
-	(let [t1 (counter)
-		t2 (counter)
-		length (- size 16)]
-		(->> field
-			(draw-pattern 8 6 [(vec (repeatedly length t1))])
-			(draw-pattern 6 8 (vec (repeatedly length #(vector (t2))))))))
+    [field]
+    (let [t1 (counter)
+        t2 (counter)
+        length (- size 16)]
+        (->> field
+            (draw-pattern 8 6 [(vec (repeatedly length t1))])
+            (draw-pattern 6 8 (vec (repeatedly length #(vector (t2))))))))
 
 
 (defn add-black-mark
-	[field]
-	(set-pixel 8 (- size 8) 3 field))
+    [field]
+    (set-pixel 8 (- size 8) 3 field))
 
 (defn add-fiding-patterns
-	[field]
-	(-> field
-		add-right-top-fp
-		add-left-top-fp
-		add-left-bottom-fp))
+    [field]
+    (-> field
+        add-right-top-fp
+        add-left-top-fp
+        add-left-bottom-fp))
 
 (defn add-alignment-patterns
-	[field]
-	field)
+    [field]
+    field)
 
 (defn reserve-information-area
-	[field]
-	(->> field
-		(draw-pattern (- size 8) 8 [(vec (repeat 8 2))])	;; top-right corner
-		(draw-pattern 8 (- size 7) (vec (repeat 7 [2])))	;; bottom-left corner
-		(draw-pattern 0 8 [(vec (repeat 6 2))])				;; top-left-horizontal
-		(draw-pattern 8 0 (vec (repeat 6 [2])))				;; top-left-vertical
-		(draw-pattern 7 7 [[2 2]							;; top-left-corner
-						   [2 2]])))
+    [field]
+    (->> field
+        (draw-pattern (- size 8) 8 [(vec (repeat 8 2))])    ;; top-right corner
+        (draw-pattern 8 (- size 7) (vec (repeat 7 [2])))    ;; bottom-left corner
+        (draw-pattern 0 8 [(vec (repeat 6 2))])                ;; top-left-horizontal
+        (draw-pattern 8 0 (vec (repeat 6 [2])))                ;; top-left-vertical
+        (draw-pattern 7 7 [[2 2]                            ;; top-left-corner
+                           [2 2]])))
 
 (defn remove-reserved
-	[field]
-	(vec (map (fn [row] (vec (map #(mod % 2) row))) field)))
+    [field]
+    (vec (map (fn [row] (vec (map #(mod % 2) row))) field)))
 
 (def empty-pixel 9)
 
 (defn- is-empty-pixel
-	[x y field]
-	(= (get-pixel x y field) empty-pixel))
+    [x y field]
+    (= (get-pixel x y field) empty-pixel))
 
 (defn- count-empty-pixels
-	[x y steps field]
-	(let [steps-count (count steps)]
-		(loop [curx-x x
-			   curx-y y
-			   counter 0
-			   result 0]
-			   (if (and (< curx-y size) (>= curx-y 0))
-			   		(let [current-step (steps (mod counter 2))
-			   			  next-x (+ curx-x (current-step 0))
-			   			  next-y (+ curx-y (current-step 1))]
-			   			  (recur next-x next-y (inc counter) (if (is-empty-pixel curx-x curx-y field) (inc result) result)))
-			   		result))))
+    [x y steps field]
+    (let [steps-count (count steps)]
+        (loop [curx-x x
+               curx-y y
+               counter 0
+               result 0]
+               (if (and (< curx-y size) (>= curx-y 0))
+                       (let [current-step (steps (mod counter 2))
+                             next-x (+ curx-x (current-step 0))
+                             next-y (+ curx-y (current-step 1))]
+                             (recur next-x next-y (inc counter) (if (is-empty-pixel curx-x curx-y field) (inc result) result)))
+                       result))))
 
 (defn add-data-step
-	[x y data steps field]
-	(let [steps-count (count steps)]
-		(loop [curx-x x
-			   curx-y y
-			   counter 0
-			   [pixel & others] data
-			   new-field field]
-			   (if (and (< curx-y size) (>= curx-y 0))
-			   		(let [cur-pixel (get-pixel curx-x curx-y new-field)
-			   			current-step (steps counter)
-			   			next-x (+ curx-x (current-step 0))
-			   			next-y (+ curx-y (current-step 1))
-			   			next-step (mod (inc counter) steps-count)]
-			   			(if (= cur-pixel empty-pixel)
-			   				(recur next-x next-y next-step others (set-pixel curx-x curx-y pixel new-field))
-			   				(recur next-x next-y next-step (into [pixel] others) new-field)))
-			   		new-field)
-			   )))
+    [x y data steps field]
+    (let [steps-count (count steps)]
+        (loop [curx-x x
+               curx-y y
+               counter 0
+               [pixel & others] data
+               new-field field]
+               (if (and (< curx-y size) (>= curx-y 0))
+                       (let [cur-pixel (get-pixel curx-x curx-y new-field)
+                           current-step (steps counter)
+                           next-x (+ curx-x (current-step 0))
+                           next-y (+ curx-y (current-step 1))
+                           next-step (mod (inc counter) steps-count)]
+                           (if (= cur-pixel empty-pixel)
+                               (recur next-x next-y next-step others (set-pixel curx-x curx-y pixel new-field))
+                               (recur next-x next-y next-step (into [pixel] others) new-field)))
+                       new-field)
+               )))
 
 (defn add-data-to-field
-	[data field]
-	(let [last-point (- size 1)
-		  directions [{
-				:steps [[-1 0] [1 -1]]
-				:y last-point 
-		  	} 
-		  	{
-			 	:steps [[-1 0] [1 1]]
-			 	:y 0
-		 	}]]
-		(loop [curx-x last-point
-			   rest-data data
-			   counter 0
-			   new-field field]
-			   (if (> (count rest-data) 0)
-			   		(let [direction (directions counter)
-			   		 	  steps (:steps direction)
-			   		 	  curx-y (:y direction)
-			   		 	  next-x (- curx-x 2)
-			   		 	  next-counter (mod (inc counter) 2)
-			   		 	  empty-pixels (count-empty-pixels curx-x curx-y steps new-field)
-			   		 	  w-data (take empty-pixels rest-data)
-			   		 	  r-data (drop empty-pixels rest-data)]
-			   				(recur next-x r-data next-counter (add-data-step curx-x curx-y w-data steps new-field)))
-			   		new-field))))
+    [data field]
+    (let [last-point (- size 1)
+          directions [{
+                :steps [[-1 0] [1 -1]]
+                :y last-point 
+              } 
+              {
+                 :steps [[-1 0] [1 1]]
+                 :y 0
+             }]]
+        (loop [curx-x last-point
+               rest-data data
+               counter 0
+               new-field field]
+               (if (> (count rest-data) 0)
+                       (let [direction (directions counter)
+                              steps (:steps direction)
+                              curx-y (:y direction)
+                              next-x (- curx-x 2)
+                              next-counter (mod (inc counter) 2)
+                              empty-pixels (count-empty-pixels curx-x curx-y steps new-field)
+                              w-data (take empty-pixels rest-data)
+                              r-data (drop empty-pixels rest-data)]
+                               (recur next-x r-data next-counter (add-data-step curx-x curx-y w-data steps new-field)))
+                       new-field))))
 
 (defn walk-matrix-row
-	[function accum y field]
-	(let [row (field y)
-		  size (count row)]
-		(loop [x 0
-			   result accum]
-			(if (< x size)
-				(recur (inc x) (function result x y))
-				result))))
+    [function accum y field]
+    (let [row (field y)
+          size (count row)]
+        (loop [x 0
+               result accum]
+            (if (< x size)
+                (recur (inc x) (function result x y))
+                result))))
 
 (defn walk-matrix
-	[function accum field]
-	(let [size (count field)]
-		(loop [y 0
-			   result accum]
-			(if (< y size)
-				(recur (inc y) (walk-matrix-row function result y field))
-				result))))
+    [function accum field]
+    (let [size (count field)]
+        (loop [y 0
+               result accum]
+            (if (< y size)
+                (recur (inc y) (walk-matrix-row function result y field))
+                result))))
 
 (defn print-field
-	[field]
-	(vec (map #(or (println %) %) field)))
+    [field]
+    (vec (map #(or (println %) %) field)))
--- test/qr/matrix_test.clj
+++ test/qr/matrix_test.clj
@@ -5,43 +5,105 @@
 (deftest bt-test
   (testing "Build Template"
     (doseq [size [21 42 56 34 33 45 100]]
-    	(let [template (build-template size)
-    		  y-size (count template)
-    		  x-sizes (map #(count %) template)]
-    		(is (= y-size size))
-    		(is (not (some #(not (= size %)) x-sizes)))
-    		(is (not (some (fn [row] (some #(not (= 9 %)) row)) template)))))))
+        (let [template (build-template size)
+              y-size (count template)
+              x-sizes (map #(count %) template)]
+            (is (= y-size size))
+            (is (not (some #(not (= size %)) x-sizes)))
+            (is (not (some (fn [row] (some #(not (= 9 %)) row)) template)))))))
 
 (deftest gp-test
   (testing "Get Pixel From Field"
     (let [field [[1  2  3  4  5  6]
-    			 [7  8  9  10 11 12]
-    			 [0  0  0  0  0  0]
-    			 [3  3  3  4  3  5]]
-		  test-data [{:x 0 :y 0 :v 1}
-		  			 {:x 1 :y 0 :v 2}
-		  			 {:x 2 :y 0 :v 3}
-		  			 {:x 3 :y 0 :v 4}
-		  			 {:x 4 :y 0 :v 5}
-		  			 {:x 5 :y 0 :v 6}
-		  			 {:x 0 :y 1 :v 7}
-		  			 {:x 5 :y 1 :v 12}
-		  			 {:x 5 :y 3 :v 5}
-		  			 {:x 3 :y 3 :v 4}
-		  			 {:x 20 :y 20 :v nil}
-		  			 {:x -1 :y 3 :v nil}]]
-    	(doseq [t test-data]
-    		(let [{x :x y :y v :v} t]
-    			(is (= v (get-pixel x y field))))))))
+                 [7  8  9  10 11 12]
+                 [0  0  0  0  0  0]
+                 [3  3  3  4  3  5]]
+          test-data [{:x 0 :y 0 :v 1}
+                       {:x 1 :y 0 :v 2}
+                       {:x 2 :y 0 :v 3}
+                       {:x 3 :y 0 :v 4}
+                       {:x 4 :y 0 :v 5}
+                       {:x 5 :y 0 :v 6}
+                       {:x 0 :y 1 :v 7}
+                       {:x 5 :y 1 :v 12}
+                       {:x 5 :y 3 :v 5}
+                       {:x 3 :y 3 :v 4}
+                       {:x 20 :y 20 :v nil}
+                       {:x -1 :y 3 :v nil}]]
+        (doseq [t test-data]
+            (let [{x :x y :y v :v} t]
+                (is (= v (get-pixel x y field))))))))
 
 (deftest sp-test
   (testing "Set Pixel From Field"
     (let [field [[0 0 0 0 0 0]
-    			 [0 0 0 0 0 0]]
-		  test-data [{:x 1 :y 1 :v 1}
-		  			 {:x 3 :y 0 :v 3}
-		  			 {:x 2 :y 1 :v 5}]]
-    	(doseq [t test-data]
-    		(let [{x :x y :y v :v} t
-    			  new-field (set-pixel x y v field)]
-    			(is (= v (get-pixel x y new-field))))))))
+                 [0 0 0 0 0 0]]
+          test-data [{:x 1 :y 1 :v 1}
+                       {:x 3 :y 0 :v 3}
+                       {:x 2 :y 1 :v 5}]]
+        (doseq [t test-data]
+            (let [{x :x y :y v :v} t
+                  new-field (set-pixel x y v field)]
+                (is (= v (get-pixel x y new-field))))))))
+
+(deftest eq-test
+	(testing "Equal Function"
+		(let [tests [{
+				:f1 [[0 0 0 0 0]]
+				:f2 [[0 0 0 0 0]]
+				:r true
+			} {
+				:f1 [[0 1 2 3 4]
+				     [5 6 7 8 9]
+				     [0 1 2 3 4]
+				     [5 6 7 8 9]]	
+			    :f2 [[0 1 2 3 4]
+				     [5 6 7 8 9]
+				     [0 1 2 3 4]
+				     [5 6 7 8 9]]
+			    :r true	
+			} {
+				:f1 [[0 1 2 7 4]	;; error
+				     [5 6 7 8 9]
+				     [0 1 2 3 4]
+				     [5 6 7 8 9]]	
+			    :f2 [[0 1 2 3 4]
+				     [5 6 7 8 9]
+				     [0 1 2 3 4]
+				     [5 6 7 8 9]]	
+			    :r false
+			}]]
+			(doseq [{f1 :f1 f2 :f2 r :r} tests]
+				(is (= r (equal f1 f2)))))))
+
+(deftest dlx-test
+    (testing "Draw X Line"
+        (let [field [[0 0 0 0 0 0]
+                     [0 0 0 0 0 0]
+                     [0 0 0 0 0 0]]
+              rfield [[0 5 5 5 5 0]
+                      [5 0 0 0 0 0]
+                      [0 0 5 5 5 0]]
+              test-data [{:x 1 :y 1 :l 0 :v 5}
+              			 {:x 1 :y 0 :l 4 :v 5}
+                         {:x 0 :y 1 :l 1 :v 5}
+                         {:x 2 :y 2 :l 3 :v 5}]]
+            (let [tfield (reduce (fn [nfield {x :x y :y l :l v :v}] (draw-line-x x y l v nfield)) field test-data)]
+            	(is (equal rfield tfield))))))
+
+(def dly-test
+    (testing "Draw Y Line"
+        (let [field [[0 0 0 0 0 0]
+                     [0 0 0 0 0 0]
+                     [0 0 0 0 0 0]
+                     [0 0 0 0 0 0]
+                     [0 0 0 0 0 0]]
+              rfield [[0 5 5 5 5 0]
+                      [5 0 0 0 0 0]
+                      [0 0 5 5 5 0]]
+              test-data [{:x 1 :y 1 :l 0 :v 5}
+              			 {:x 1 :y 0 :l 4 :v 5}
+                         {:x 0 :y 1 :l 1 :v 5}
+                         {:x 2 :y 2 :l 3 :v 5}]]
+            (let [tfield (reduce (fn [nfield {x :x y :y l :l v :v}] (draw-line-x x y l v nfield)) field test-data)]
+            	(is (equal rfield tfield))))))
